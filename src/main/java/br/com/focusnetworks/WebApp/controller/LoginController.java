1d29f70146ab6387a7adaaf2c39182564d2bc276
Elton Alex Silva <eltonalex.s@gmail.com>
2019-03-27 16:53:23 -0300

    Atualização do projeto com template, validações e mensagens de retorno.

---
+++ src/main/java/br/com/focusnetworks/WebApp/controller/LoginController.java
@@ -0,0 +1,55 @@
+package br.com.focusnetworks.WebApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.focusnetworks.WebApp.model.City;
+import br.com.focusnetworks.WebApp.model.Login;
+import br.com.focusnetworks.WebApp.repository.LoginRepository;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	@Autowired
+	private LoginRepository loginRepository;
+
+	public LoginController(LoginRepository loginRepository) {
+		super();
+		this.loginRepository = loginRepository;
+	}
+	
+	@GetMapping
+	public String index(@ModelAttribute("login")Login login,  Model model) {
+		model.addAttribute("login", new Login());
+		return "login/index";
+	}
+	
+	@PostMapping("/confirm")
+	public String confirm(@ModelAttribute("login")Login login,  Model model) {
+		model.addAttribute("username",login.getUsername());
+		model.addAttribute("email",login.getEmail());
+		model.addAttribute("password",login.getPassword());
+		
+		return "login/confirm";
+	}
+	
+	@PostMapping("/")
+	public String create(@ModelAttribute("login") Login login, Model model) {
+		this.loginRepository.save(login);
+		return "redirect:/login/success";
+	}
+	
+	@GetMapping("/success")
+	public String success(@ModelAttribute("login")Login login,  Model model) {
+		model.addAttribute("login", new Login());
+		return "login/success";
+	}
+
+}
---
+++ src/main/java/br/com/focusnetworks/WebApp/model/Login.java
@@ -0,0 +1,70 @@
+package br.com.focusnetworks.WebApp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Login {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String username;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String password;
+	
+	public Login() {}
+
+	public Login(String username, String email, String password) {
+		super();
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+}
