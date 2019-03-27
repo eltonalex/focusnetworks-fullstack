aa695c833db5e034693ede0962fe9100a266e083
Elton Alex Silva <eltonalex.s@gmail.com>
2019-03-27 17:30:09 -0300

    Atualização do projeto. Retirada de páginas e ajuste de mensagem.

--- src/main/java/br/com/focusnetworks/WebApp/controller/LoginController.java
+++ src/main/java/br/com/focusnetworks/WebApp/controller/LoginController.java
@@ -5,18 +5,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.focusnetworks.WebApp.model.City;
 import br.com.focusnetworks.WebApp.model.Login;
 import br.com.focusnetworks.WebApp.repository.LoginRepository;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-	
+
 	@Autowired
 	private LoginRepository loginRepository;
 
@@ -24,32 +22,43 @@ public class LoginController {
 		super();
 		this.loginRepository = loginRepository;
 	}
-	
+
 	@GetMapping
-	public String index(@ModelAttribute("login")Login login,  Model model) {
+	public String index(@ModelAttribute("login") Login login, Model model) {
 		model.addAttribute("login", new Login());
 		return "login/index";
 	}
-	
+
 	@PostMapping("/confirm")
-	public String confirm(@ModelAttribute("login")Login login,  Model model) {
-		model.addAttribute("username",login.getUsername());
-		model.addAttribute("email",login.getEmail());
-		model.addAttribute("password",login.getPassword());
-		
-		return "login/confirm";
+	public String confirm(@ModelAttribute("login") Login login, Model model) {
+
+		if (login.getUsername().length() > 0 && login.getEmail().length() > 0 && login.getPassword().length() > 0) {
+
+			model.addAttribute("username", login.getUsername());
+			model.addAttribute("email", login.getEmail());
+			model.addAttribute("password", login.getPassword());
+
+			return "login/confirm";
+		}
+		return "login/error";
 	}
-	
+
 	@PostMapping("/")
 	public String create(@ModelAttribute("login") Login login, Model model) {
 		this.loginRepository.save(login);
 		return "redirect:/login/success";
 	}
-	
+
 	@GetMapping("/success")
-	public String success(@ModelAttribute("login")Login login,  Model model) {
+	public String success(@ModelAttribute("login") Login login, Model model) {
 		model.addAttribute("login", new Login());
 		return "login/success";
+	}
+
+	@GetMapping("/error")
+	public String error(@ModelAttribute("login") Login login, Model model) {
+		model.addAttribute("login", new Login());
+		return "login/error";
 	}
 
 }
